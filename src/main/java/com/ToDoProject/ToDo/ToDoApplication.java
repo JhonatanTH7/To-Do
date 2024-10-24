78e30bd28eb6958369046bbed8a765143be9a8e9
JhonatanTH7 <jhonatantorohurtado93@gmail.com>
2024-10-24 04:49:37 -0500

    Configs

--- src/main/java/com/ToDoProject/ToDo/ToDoApplication.java
+++ src/main/java/com/ToDoProject/ToDo/ToDoApplication.java
@@ -4,15 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 @EnableJpaAuditing
 public class ToDoApplication {
 
 	public static void main(String[] args) {
-		@SuppressWarnings("unused")
-		Dotenv dotenv = Dotenv.load();
 		SpringApplication.run(ToDoApplication.class, args);
 	}
 
